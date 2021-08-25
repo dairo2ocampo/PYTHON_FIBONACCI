10991910a51a02c9ab35f935cbfb1396f39d0f7f
dairo2ocampo <ibaguenet@hotmail.com>
2021-08-25 06:07:51 -0500

    lo primero que estoy haciendo

---
+++ index1.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<title>Pagina de pruebas</title>
+</head>
+
+<body>
+<?php
+  
+    $cadena = "<h1>dairo</h1>";
+    echo filter_var($cadena,FILTER_SANITIZE_STRING);   
+
+?>
+    <form action="validar.php" method="POST" enctype="multipart/form-data">
+    <input type="text" name="nombre">
+    <br><input type="file" name="arch[]">
+    <br><input type="file" name="arch[]">
+    <br><input type="file" name="arch[]">
+    <br><input type="submit" name="ok">
+    </form>
+
+
+
+
+</body>
+</html>
